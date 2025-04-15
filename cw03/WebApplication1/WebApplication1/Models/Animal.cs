77efc0a9f83bda9bd5856751e8b3caea70bfbc93
Artur Kowalski <arturgrzegorzkowalski@gmail.com>
2025-04-16 00:15:58 +0200

    changes

---
+++ cw03/WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models;
+
+public class Animal
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string category { get; set; }
+    public double weight { get; set; }
+    public string color { get; set; }
+}
